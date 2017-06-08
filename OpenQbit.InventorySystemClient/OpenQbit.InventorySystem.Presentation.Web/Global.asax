22a2e933539b914a140c7a4d2e50f837cbda9dac
Anjana Thrishakta <anjanathrishakya@gmail.com>
2017-06-08 14:10:47 +0530

    Create Client Project

---
+++ OpenQbit.InventorySystemClient/OpenQbit.InventorySystem.Presentation.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OpenQbit.InventorySystem.Presentation.Web.MvcApplication" Language="C#" %>
---
+++ OpenQbit.InventorySystemClient/OpenQbit.InventorySystem.Presentation.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OpenQbit.InventorySystem.Presentation.Web.Startup))]
+namespace OpenQbit.InventorySystem.Presentation.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
